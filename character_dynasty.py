d45f359988fea33630fcd0d9c2ac244fab99ef9d
captainslapster <hdredfox@gmail.com>
2024-07-23 16:04:15 -0400

    Created first iteration of dynasty creation. It will be simple, but it should work.

---
+++ character_dynasty.py
@@ -0,0 +1,21 @@
+##Dynasty creation code we'll see how it works##
+#
+#               X---X Gen 1
+#             X--X X--X Gen 2
+#           X-X X-X X-X X-X Gen 3
+#
+# Dynasties will be classes in order to make creating/adjusting easier
+#
+#
+#
+
+
+class Dynasty():
+
+    def __init__(self,id):
+        self.id = id # Dynasty ID
+        self.maxsize = 14 #14 for testing purposes    
+        
+    def createGeneration():
+        
+        pass
