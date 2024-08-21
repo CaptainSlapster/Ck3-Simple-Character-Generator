f96fbb937774a05fdd0df2d7aebaa006241a2532
captainslapster <hdredfox@gmail.com>
2024-08-21 18:49:33 -0400

    removed debug stuff

--- Character_gen.py
+++ Character_gen.py
@@ -24,10 +24,6 @@ with open('outfile.txt','w+',encoding= 'utf-8')as out:
     
     for i in range(1,number_of_characters):
         char = C.create_character(i,start_year,min_age,max_age,religion,culture,percent_female,trait_number,names_male,names_female,trait_list,min_date_range,max_date_range)
-        print("birth year: " + str(char.birthyear))
-        print("age: " + str(char.age))
-        print("death year: " + str(char.deathyear))
-        
         generate_text(out,char)
 
 
